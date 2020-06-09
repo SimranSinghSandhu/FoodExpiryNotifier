c7afdd0c1ca0f59a7fa3dd717348da2dec3f2645
Simran <sam.sandhu131@gmail.com>
2020-06-09 21:08:21 +0530

    Creating New item on Add Button Pressed, also textfield of  latest Cell created becomes first responder

---
+++ FoodExpiryNotifier/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  FoodExpiryNotifier
+//
+//  Created by Simran Singh Sandhu on 09/06/20.
+//  Copyright © 2020 Simran Singh Sandhu. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    
+    var name: String?
+    
+}
--- FoodExpiryNotifier/ViewController.swift
+++ FoodExpiryNotifier/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     let cellID = "cellID"
     
+    var items = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,18 +34,26 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! CustomCell
         
-        cell.nameTextField.text = "Batman"
+//        cell.nameTextField.text = ""
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75
+    }
+    
+    // When Cell Loads.
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if let cell = cell as? CustomCell {
+            // Latest Cell Becomes First Responder.
+            cell.nameTextField.becomeFirstResponder()
+        }
     }
 }
 
@@ -58,5 +68,26 @@ extension ViewController {
     // To Add new Item in the TableView.
     @objc func addBtnHandle() {
         print("Add Button Pressed!")
+        addingNewItem()
     }
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    private func addingNewItem() {
+        
+        // Creating Indexpath for Inserting Cell in TableView
+        let indexPath: IndexPath = [0, items.count]
+        
+        let newItem = Item() // Creating Instance of Items Array
+        
+        newItem.name = "" // Default Text
+        items.append(newItem) // Appending the NewItem inside out Array of Items Object
+        
+        // Updating the tableView.
+        tableView.beginUpdates()
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        tableView.endUpdates()
+    }
+    
 }
