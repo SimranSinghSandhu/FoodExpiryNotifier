bbc65dc42dd22535e71b02714b69368931e20d3f
Simran <sam.sandhu131@gmail.com>
2020-06-09 18:54:26 +0530

    Creating UItableView and its Constraints in Storyboard and Setting up the delegates and datasource Functions, creating 5 cells for demo.

--- FoodExpiryNotifier/ViewController.swift
+++ FoodExpiryNotifier/ViewController.swift
@@ -9,5 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellID = "cellID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = UIColor.systemBlue
+        return cell
+    }
 }
