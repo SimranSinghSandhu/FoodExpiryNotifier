0a733f14809c0b7add24b1e71feafa2ccbe20b57
Simran <sam.sandhu131@gmail.com>
2020-06-14 08:28:35 +0530

    Adding Info Button as a RightView on Textfield, Visible only when Textfield is editing.

--- FoodExpiryNotifier/CustomCell.swift
+++ FoodExpiryNotifier/CustomCell.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class CustomCell: UITableViewCell {
-    
+
     let nameTextField: UITextField = {
         let textField = UITextField()
+        textField.placeholder = "Enter new Item..."
+        
+        textField.clipsToBounds = true
+        textField.rightViewMode = .whileEditing
         textField.font = UIFont.boldSystemFont(ofSize: 24)
         textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
@@ -26,15 +30,35 @@ class CustomCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         addSubview(nameTextField)
+        
         settingConstraints()
+        
+        settingRightView()
+        
         layoutIfNeeded()
     }
     
+    private func settingRightView() {
+        let infoBtn = UIButton(frame: CGRect(x: 0, y: 0, width: 75, height: 75))
+        infoBtn.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        infoBtn.addTarget(self, action: #selector(infoBtnHandle), for: .touchUpInside)
+        infoBtn.backgroundColor = UIColor.systemBlue
+        nameTextField.rightView = infoBtn
+    }
+    
+    @objc func infoBtnHandle(){
+        print("Info button is pressed!")
+    }
     
     private func settingConstraints() {
+        
+        let width = UIScreen.main.bounds.size.width
+        let padding: CGFloat = 20
+
         nameTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
         nameTextField.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 0).isActive = true
-        nameTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        nameTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        nameTextField.heightAnchor.constraint(equalToConstant: self.frame.height).isActive = true
+        nameTextField.widthAnchor.constraint(equalToConstant: width - padding).isActive = true
+        
     }
 }
